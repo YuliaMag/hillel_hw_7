9ca665a08e47bf1e4d47cac353024aefac95bc79
Yulia Magonya <ymagonia@gmail.com>
2024-05-08 21:05:30 +0300

    hw2.low_try_

--- LECT7_HW2.py
+++ LECT7_HW2.py
@@ -1,4 +1,9 @@
 
 date_s = ["14-Dec", "12-Apr", "13-Apr", "31-Dec", "01-Jan", "12-Jan"]
 
+
 print(sorted(date_s))
+
+print(sorted(date_s, key=lambda date: [d for d in date.split("-")][0]))
+
+print(sorted(date_s, key=lambda date: [d for d in date.split("-")][1]))
