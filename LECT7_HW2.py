558e9e802bc3e88c4e8fa7a7a6cef3a22ecfb14f
Yulia Magonya <ymagonia@gmail.com>
2024-05-09 01:07:15 +0300

    hw2.big_hopes

--- LECT7_HW2.py
+++ LECT7_HW2.py
@@ -1,9 +1,30 @@
+def mnt(x):
+    months = {
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12
+    }
+    a = x.strip()[:3].lower()
+    try:
+        e = months[a]
+        return e
+    except:
+        raise ValueError('Not a month')
+# ========================================================= #
 
-date_s = ["14-Dec", "12-Apr", "13-Apr", "31-Dec", "01-Jan", "12-Jan"]
+
+date_s = ["14-Dec", "12-Apr", "13-Apr", "31-Dec", "1-Jan", "12-Jan"]
+
+print(sorted(date_s, key=lambda m: (mnt(m.split("-")[1]))))
 
 
-print(sorted(date_s))
 
-print(sorted(date_s, key=lambda date: [d for d in date.split("-")][0]))
-
-print(sorted(date_s, key=lambda date: [d for d in date.split("-")][1]))
