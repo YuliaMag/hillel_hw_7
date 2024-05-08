270b1a06c07e620f62b1748a36b519c8f7a929c0
Yulia Magonya <ymagonia@gmail.com>
2024-05-08 13:20:56 +0300

    hw1-correct

--- LECT7_HW1.py
+++ LECT7_HW1.py
@@ -1,10 +1,25 @@
-def lst2dict():
-    lst = input("Enter the list: ")
+# def lst2dict():
+#     lst = input("Enter the list: ")
+#     lst1 = lst[::2]
+#     lst2 = lst[1::2]
+#     zip_ = zip(lst1, lst2)
+#     return dict(zip_)
+#
+#
+# answ = lst2dict()
+# print(answ)
+
+def lst2dict(lst):
     lst1 = lst[::2]
     lst2 = lst[1::2]
     zip_ = zip(lst1, lst2)
     return dict(zip_)
 
 
-answ = lst2dict()
-print(answ)
+lst = input("Enter the list: ")
+lst2dict(lst)
+print(lst2dict(lst))
+
+
+
+
